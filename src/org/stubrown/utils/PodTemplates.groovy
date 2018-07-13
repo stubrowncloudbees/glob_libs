b7ef56d73d5dd637e34a0e871afb77a390e5769d
stu <sbrown@cloudbees.com>
2018-07-13 10:02:01 +0100

    adding in global library

---
+++ src/org/stubrown/utils/PodTemplates.groovy
@@ -0,0 +1,20 @@
+package com.foo.utils
+
+public void dockerTemplate(body) {
+    podTemplate(label: label,
+            containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
+            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+        body()
+    }
+}
+
+public void mavenTemplate(body) {
+    podTemplate(label: label,
+            containers: [containerTemplate(name: 'maven', image: 'maven', command: 'cat', ttyEnabled: true)],
+            volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
+                      persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo')]) {
+        body()
+    }
+}
+
+return this
