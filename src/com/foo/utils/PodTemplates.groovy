a487edb06c9a0b5899d40ee60d3388bfde7db637
stu <sbrown@cloudbees.com>
2018-07-13 11:23:56 +0100

    fixing up package path

--- src/com/foo/utils/PodTemplates.groovy
+++ src/com/foo/utils/PodTemplates.groovy
@@ -1,7 +1,7 @@
 package com.foo.utils
 
 public void dockerTemplate(body) {
-    podTemplate(label: label,
+    podTemplate(label: 'label',
             containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
         body()
@@ -9,7 +9,7 @@ public void dockerTemplate(body) {
 }
 
 public void mavenTemplate(body) {
-    podTemplate(label: label,
+    podTemplate(label: 'label',
             containers: [containerTemplate(name: 'maven', image: 'maven', command: 'cat', ttyEnabled: true)],
             volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
                       persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo')]) {
