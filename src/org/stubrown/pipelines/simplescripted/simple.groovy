e587c045eaf705b07bc4ae85a142af2909c85530
stu <sbrown@cloudbees.com>
2018-07-30 08:34:00 +0100

    simplescripted libs

---
+++ src/org/stubrown/pipelines/simplescripted/simple.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/groovy
+package org.stubrown.pipelines.simplescripted;
+
+def build() {
+    sh "building ${name}"
+}
+def test(name) {
+    sh "echo testing ${name}"
+}
+def deploy(env,app) {}
+   sh "deploying ${name} to ${app}"
+// AimTheory have a recommendation and explanation about this here
+    return this
